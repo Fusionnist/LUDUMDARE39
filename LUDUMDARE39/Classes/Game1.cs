b1804dd2d13af0c12839a3619a179fd527c27862
Fusionnist <antoine.chartier@gmail.com>
2017-07-29 14:16:44 +0200

    graphics

--- LUDUMDARE39/Classes/Game1.cs
+++ LUDUMDARE39/Classes/Game1.cs
@@ -9,15 +9,36 @@ namespace LUDUMDARE39
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Player player;
+        RenderTarget2D target;
+        float scale;
+        Point virtualDim;
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            graphics.PreferredBackBufferHeight = 1080;
-            graphics.PreferredBackBufferWidth = 1920;
-        }
+            graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+            graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+            Window.IsBorderless = true;
+            graphics.ApplyChanges();
 
+            SetupGraphics();
+        }
+        void SetupGraphics()
+        {
+            virtualDim = new Point(192,108);
+
+            float xscale = GraphicsDevice.Viewport.Width / virtualDim.X;
+            float yscale = GraphicsDevice.Viewport.Height / virtualDim.Y;
+
+            if (xscale > yscale)
+            {
+                scale = yscale;
+            }
+            else { scale = xscale; }
+
+            target = new RenderTarget2D(GraphicsDevice, virtualDim.X, virtualDim.Y);
+        }
         protected override void Initialize()
         {
             base.Initialize();        
@@ -46,11 +67,16 @@ namespace LUDUMDARE39
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
+            GraphicsDevice.SetRenderTarget(target);
             spriteBatch.Begin();
             player.Draw(spriteBatch);
             spriteBatch.End();
 
+            Matrix m = Matrix.CreateScale(scale);
+            GraphicsDevice.SetRenderTarget(null);
+            spriteBatch.Begin(transformMatrix:m);
+            spriteBatch.Draw(texture: target,position: Vector2.Zero);
+            spriteBatch.End();
             base.Draw(gameTime);
         }
     }

9b5e3bc8f6e1d0a4ac89c22af3a7cd058a6281e4
Louis Vassaux <louis.vassaux@sfr.fr>
2017-07-29 12:27:02 +0200

    basic stuff

--- LUDUMDARE39/Classes/STexture.cs
+++ LUDUMDARE39/Classes/STexture.cs
@@ -11,6 +11,40 @@ namespace LUDUMDARE39
 {
     class STexture
     {
+        Texture2D tex;
+        int framecount, framelength, currentframe;
+        float frametime, frametimer;
 
+        public STexture(Texture2D a_tex)
+        {
+            tex = a_tex;
+        }
+
+        public STexture(Texture2D a_tex, int a_FC, int a_FL, int a_FT)
+        {
+            tex = a_tex;
+            framecount = a_FC;
+            framelength = a_FL;
+            frametime = a_FT;
+            currentframe = 1;
+            frametimer = frametime;
+        }
+
+        public void Update(GameTime a_gt)
+        {
+            frametimer -= (float)a_gt.ElapsedGameTime.TotalSeconds;
+            if (frametimer <= 0)
+            {
+                frametimer = frametime;
+                currentframe++;
+                if (currentframe > framecount)
+                    currentframe = 1;
+            }
+        }
+
+        public void Draw(SpriteBatch a_sb, Vector2 a_pos)
+        {
+            a_sb.Draw(tex, a_pos, sourceRectangle: new Rectangle(framelength * (currentframe - 1), 0, framelength, tex.Height));
+        }
     }
 }

2e5c712b514b7fdcc5c1c41aed04554875b6af7c
Louis Vassaux <louis.vassaux@sfr.fr>
2017-07-29 12:02:43 +0200

    qsd

--- LUDUMDARE39/Classes/Sprite.cs
+++ LUDUMDARE39/Classes/Sprite.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace LUDUMDARE39
 {
     class Sprite
     {
+        STexture tex;
+        Vector2 pos;
+
+        public Sprite(STexture a_tex, Vector2 a_pos)
+        {
+            tex = a_tex;
+            pos = a_pos;
+        }
     }
 }

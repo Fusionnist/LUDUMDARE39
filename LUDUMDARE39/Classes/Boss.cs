478d818504ebea2b299fa9f8ea22bc551c39328f
Fusionnist <antoine.chartier@gmail.com>
2017-07-29 18:31:25 +0200

    s

--- LUDUMDARE39/Classes/Boss.cs
+++ LUDUMDARE39/Classes/Boss.cs
@@ -19,7 +19,7 @@ namespace LUDUMDARE39
         Vector2 nearestPlug;
         public float hp, maxhp;
 
-        public Boss(STexture[] a_tex, Vector2 a_pos): base(a_tex, a_pos)
+        public Boss(STexture[] a_tex, Vector2 a_pos) : base(a_tex, a_pos)
         {
             SetPlatformData();
             movType = BossMovement.Right;
@@ -37,16 +37,12 @@ namespace LUDUMDARE39
         }
         public void Move(GameTime a_gt)
         {
-            
+
         }
         void SeekPlug(GameTime a_gt)
         {
-            if(nearestPlug.X - pos.X < 1 && nearestPlug.X - pos.X > -1) { pos.X = nearestPlug.X; }
-            else
-            {
-                if (nearestPlug.X > pos.X) { mov.X += 10 * (float)a_gt.ElapsedGameTime.TotalSeconds; }//temp
-                if (nearestPlug.X < pos.X) { mov.X += -10 * (float)a_gt.ElapsedGameTime.TotalSeconds; }//temp
-            }
+            if (nearestPlug.X > pos.X) { mov.X += 10 * (float)a_gt.ElapsedGameTime.TotalSeconds; }//temp
+            if (nearestPlug.X < pos.X) { mov.X += -10 * (float)a_gt.ElapsedGameTime.TotalSeconds; }//temp
         }
         void Shoot()
         {
@@ -54,37 +50,36 @@ namespace LUDUMDARE39
         }
         public void Update(GameTime a_gt, Rectangle virtualDims, Switch[] switches_, Vector2 playerpos_)
         {
+            isPlugged = false;
             shotTimer -= (float)a_gt.ElapsedGameTime.TotalSeconds;
-            if(shotTimer <= 0) { shotTimer = shotTime; Shoot(); }
-
-            if (!isPlugged)
+            if (shotTimer <= 0) { shotTimer = shotTime; Shoot(); }
+            float record = 1000;
+            nearestPlug = pos;
+            foreach (Switch s in switches_)
             {
-                hp -= 10 * (float)a_gt.ElapsedGameTime.TotalSeconds;
-                float record = 1000;
-                nearestPlug = pos;
-                foreach (Switch s in switches_)
+                if (s.isOn)
                 {
-                    if (s.isOn)
+                    if ((Vector2.Distance(pos, s.plug.pos)) < record)
                     {
-                        if ((Vector2.Distance(pos, s.plug.pos)) < record)
-                        {
-                            record = Vector2.Distance(pos, s.plug.pos);
-                            nearestPlug = s.plug.pos;
-                        }
+                        record = Vector2.Distance(pos, s.plug.pos);
+                        nearestPlug = s.plug.pos;
+                        if (nearestPlug.X - pos.X < 1 && nearestPlug.X - pos.X > -1) { pos.X = nearestPlug.X; isPlugged = true; }
+                        
                     }
                 }
-                SeekPlug(a_gt);
+                if (!isPlugged) { hp -= 10 * (float)a_gt.ElapsedGameTime.TotalSeconds; SeekPlug(a_gt); }
+                else
+                {
+                    hp += 8 * (float)a_gt.ElapsedGameTime.TotalSeconds;
+                }
+
+                pos += mov;
+                mov = Vector2.Zero;
+
+                SetPlatformData();
+
+                base.Update(a_gt);
             }
-            else {
-                hp += 8 * (float)a_gt.ElapsedGameTime.TotalSeconds;
-            }
-
-            pos += mov;
-            mov = Vector2.Zero;
-
-            SetPlatformData();
-
-            base.Update(a_gt);
         }
     }
 }

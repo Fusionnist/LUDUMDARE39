cd2202ab3b51243d0f7b2324eedcdfb1bae0d055
Fusionnist <antoine.chartier@gmail.com>
2017-07-29 18:10:09 +0200

    bullets!?

--- LUDUMDARE39/Classes/Bullet.cs
+++ LUDUMDARE39/Classes/Bullet.cs
@@ -12,9 +12,33 @@ namespace LUDUMDARE39
     public class Bullet:Sprite
     {
         int bounces;
-        public Bullet():base(null,Vector2.Zero)
-        {
+        Vector2 velocity, baseVelocity;
+        float lifeTime;
+        bool lifeTimed;
+        Vector2 velLoss;
+        Vector2 mov;
+        Point xyDir;
+        bool isExploding, isDead;
 
+        public Bullet(STexture[] texes_, Vector2 pos_, int bounces_,float lifet_, bool lifeTD_, Vector2 vel_, Vector2 velLoss_, Point xyDir_):base(texes_, pos_)
+        {
+            bounces = bounces_;
+            velocity = vel_; baseVelocity = velocity;
+            lifeTime = lifet_;
+            lifeTimed = lifeTD_;
+            velLoss = velLoss_;
+            xyDir = xyDir_;
+        }
+        public void CalculateTarget(Vector2 tar_, float timeToTar_)
+        {
+            velocity.X = tar_.X - pos.X / timeToTar_;
+            velLoss.X = 0;
+        }
+        public void Update(GameTime a_gt, Rectangle vdims_)
+        {
+            base.Update(a_gt);
+        
+            if (truehb.Y > vdims_.Y + vdims_.Height) { }
         }
     }
 }
